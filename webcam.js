fc042b7a1ab30567ecbee2f7bfefcc4e34488ebb
Mr Ruba <zacharyjcanderson@gmail.com>
2018-01-13 17:01:46 -0800

    Sends the img in base 64 from canvas through a post request

--- webcam.js
+++ webcam.js
@@ -14,6 +14,8 @@ button.onclick = function () {
     canvas.width = video.videoWidth;
     canvas.height = video.videoHeight;
     canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
+    var img = getImg(canvas.toDataURL());
+    post('/', {value: img});
 };
 
 var constraints = {
@@ -33,3 +35,31 @@ function handleError(error) {
 navigator.mediaDevices.getUserMedia(constraints)
     .then(handleSuccess)
     .catch(handleError);
+
+function getImg(dataURL){
+      return img.replace(/^data:image\/\w+;base64,/, "");
+}
+
+function post(path, params, method) {
+    method = method || "post"; // Set method to post by default if not specified.
+
+    // The rest of this code assumes you are not using a library.
+    // It can be made less wordy if you use one.
+    var form = document.createElement("form");
+    form.setAttribute("method", method);
+    form.setAttribute("action", path);
+
+    for(var key in params) {
+        if(params.hasOwnProperty(key)) {
+            var hiddenField = document.createElement("input");
+            hiddenField.setAttribute("type", "hidden");
+            hiddenField.setAttribute("name", key);
+            hiddenField.setAttribute("value", params[key]);
+
+            form.appendChild(hiddenField);
+        }
+    }
+
+    document.body.appendChild(form);
+    form.submit();
+}
